92dfabbd4c34e20e6d586cb02256b9e07d19a020
MaxJXiao <80502041+MaxJXiao@users.noreply.github.com>
2021-05-13 13:22:47 +1000

    Add files via upload

--- Functions.jl
+++ Functions.jl
@@ -12,17 +12,6 @@ function Laplacian(😄,A,Δx)
     return P
 end
 
-function Laplacian_tensor(😄,A,Δx)
-    B = CircularArray(A);
-    P = zeros(😄,😄);
-    @tullio P[i,j] := (-B[i+2,j] + 16B[i+1,j] 
-    + 16B[i-1,j] -B[i-2,j] 
-    -B[i,j+2] + 16B[i,j+1] 
-    + 16B[i,j-1] -B[i,j-2] 
-    - 60A[i,j]) /(12Δx^2);
-    return P
-end
-
 function Laplacian_circle(😄,A,Δx)
     B = CircularArray(A);
     P = zeros(😄,😄);
@@ -38,15 +27,15 @@ function Laplacian_circle(😄,A,Δx)
     return P
 end
 
-function Laplacian_3D(😄,A,Δx)
+function Laplacian_3D(😄::Int16,A::AbstractArray{Float16},Δx::Float16,P::AbstractArray{Float16})
+
     B = CircularArray(A);
-    P = zeros(😄,😄,😄);
-    for i = 1: 😄
-        for j = 1: 😄
-            for k = 1: 😄
+    for i = 1:😄
+        for j = 1:😄
+            for k = 1:😄
                 P[i,j,k] = (-B[i+2,j,k] + 16B[i+1,j,k] + 16B[i-1,j,k] - B[i-2,j,k]
                 -B[i,j+2,k] + 16B[i,j+1,k] + 16B[i,j-1,k] - B[i,j-2,k]
-                -B[i,j,k+2] + 16B[i,j,k+1] + 16B[i,j,k-1] - B[i,j,k-2] - 90A[i,j,k])/(12Δx^2)
+                -B[i,j,k+2] + 16B[i,j,k+1] + 16B[i,j,k-1] - B[i,j,k-2] - 90B[i,j,k])/(12Δx^2)
             end
         end
     end
@@ -162,7 +151,7 @@ function update_4(N,A₁,A₂,Ȧ₁,Ȧ₂,ω,η,Δx,Δt,time)
 
 end
 
-function update_5(N,A₁,A₂,Ȧ₁,Ȧ₂,ω,η,Δx,Δt,time)
+function update_6(N,A₁::AbstractArray{Float16},A₂::AbstractArray{Float16},Ȧ₁::AbstractArray{Float16},Ȧ₂::AbstractArray{Float16},ω,η,Δx,Δt,time,P::AbstractArray{Float16})
     a(t) = 4.36000000000006e-18*t - 6.78288102293483e-23
 
     β = 0
@@ -170,22 +159,22 @@ function update_5(N,A₁,A₂,Ȧ₁,Ȧ₂,ω,η,Δx,Δt,time)
     © = 1
     λ = 2π^2/ω^2
 
-    F₁ = Laplacian_tensor(N,A₁,Δx) .- a(time).^β * λ .* A₁ .* (A₁.^2 .+ A₂.^2 .- η^2) .- α * © .* Ȧ₁ ./ time
-    F₂ = Laplacian_tensor(N,A₂,Δx) .- a(time).^β * λ .* A₂ .* (A₁.^2 .+ A₂.^2 .- η^2) .- α * © .* Ȧ₂ ./ time
+    F₁ = Laplacian_3D(N,A₁,Δx,P) .- a(time).^β * λ .* A₁ .* (A₁.^2 .+ A₂.^2 .- η^2) .- α * © .* Ȧ₁ ./ time
+    F₂ = Laplacian_3D(N,A₂,Δx,P) .- a(time).^β * λ .* A₂ .* (A₁.^2 .+ A₂.^2 .- η^2) .- α * © .* Ȧ₂ ./ time
     
     lime = time + Δt
 
     B₁ = A₁ .+ Δt * (Ȧ₁ .+ 0.5Δt * F₁)
     B₂ = A₂ .+ Δt * (Ȧ₂ .+ 0.5Δt * F₂)
 
-    Ḃ₁ = Ȧ₁ .+ 0.5Δt .* (F₁ .+ Laplacian_tensor(N,B₁,Δx) .- a(lime).^β * λ .* B₁ .* (B₁.^2 .+ B₂.^2 .- η^2) .- α * © .* Ȧ₁ ./ lime)
-    Ḃ₂ = Ȧ₂ .+ 0.5Δt .* (F₂ .+ Laplacian_tensor(N,B₂,Δx) .- a(lime).^β * λ .* B₂ .* (B₁.^2 .+ B₂.^2 .- η^2) .- α * © .* Ȧ₂ ./ lime)
+    Ḃ₁ = Ȧ₁ .+ 0.5Δt .* (F₁ .+ Laplacian_3D(N,B₁,Δx,P) .- a(lime).^β * λ .* B₁ .* (B₁.^2 .+ B₂.^2 .- η^2) .- α * © .* Ȧ₁ ./ lime)
+    Ḃ₂ = Ȧ₂ .+ 0.5Δt .* (F₂ .+ Laplacian_3D(N,B₂,Δx,P) .- a(lime).^β * λ .* B₂ .* (B₁.^2 .+ B₂.^2 .- η^2) .- α * © .* Ȧ₂ ./ lime)
 
     return B₁,B₂,Ḃ₁,Ḃ₂,lime
 
 end
 
-function update_6(N,A₁,A₂,Ȧ₁,Ȧ₂,ω,η,Δx,Δt,time)
+function update_7(N,A₁,A₂,Ȧ₁,Ȧ₂,M₁,M₂,ω,η,Δx,Δt,time)
     a(t) = 4.36000000000006e-18*t - 6.78288102293483e-23
 
     β = 0
@@ -193,18 +182,21 @@ function update_6(N,A₁,A₂,Ȧ₁,Ȧ₂,ω,η,Δx,Δt,time)
     © = 1
     λ = 2π^2/ω^2
 
-    F₁ = Laplacian_3D(N,A₁,Δx) .- a(time).^β * λ .* A₁ .* (A₁.^2 .+ A₂.^2 .- η^2) .- α * © .* Ȧ₁ ./ time
-    F₂ = Laplacian_3D(N,A₂,Δx) .- a(time).^β * λ .* A₂ .* (A₁.^2 .+ A₂.^2 .- η^2) .- α * © .* Ȧ₂ ./ time
+    F₁ = M₁ .- α * © .* Ȧ₁ ./ time
+    F₂ = M₂ .- α * © .* Ȧ₂ ./ time
     
     lime = time + Δt
 
     B₁ = A₁ .+ Δt * (Ȧ₁ .+ 0.5Δt * F₁)
     B₂ = A₂ .+ Δt * (Ȧ₂ .+ 0.5Δt * F₂)
 
-    Ḃ₁ = Ȧ₁ .+ 0.5Δt .* (F₁ .+ Laplacian_3D(N,B₁,Δx) .- a(lime).^β * λ .* B₁ .* (B₁.^2 .+ B₂.^2 .- η^2) .- α * © .* Ȧ₁ ./ lime)
-    Ḃ₂ = Ȧ₂ .+ 0.5Δt .* (F₂ .+ Laplacian_3D(N,B₂,Δx) .- a(lime).^β * λ .* B₂ .* (B₁.^2 .+ B₂.^2 .- η^2) .- α * © .* Ȧ₂ ./ lime)
+    L₁ = Laplacian_circle(N,B₁,Δx) .- a(lime).^β * λ .* B₁ .* (B₁.^2 .+ B₂.^2 .- η^2)
+    L₂ = Laplacian_circle(N,B₂,Δx) .- a(lime).^β * λ .* B₂ .* (B₁.^2 .+ B₂.^2 .- η^2)
 
-    return B₁,B₂,Ḃ₁,Ḃ₂,lime
+    Ḃ₁ = Ȧ₁ .+ 0.5Δt .* (F₁ .+ L₁ .- α * © .* Ȧ₁ ./ lime)
+    Ḃ₂ = Ȧ₂ .+ 0.5Δt .* (F₂ .+ L₂ .- α * © .* Ȧ₂ ./ lime)
+
+    return B₁,B₂,Ḃ₁,Ḃ₂,L₁,L₂,lime
 
 end
 
@@ -214,7 +206,7 @@ function run(N,t₀,t,A₁,A₂,Ȧ₁,Ȧ₂,ω,η,Δx,Δt)
     time = t₀;
     steps = round(t/Δt,digits = 0);
     
-    for i in 1:steps
+    for _ ∈ 1:steps
         time = round(time,digits = 1);
         A₁,A₂,Ȧ₁,Ȧ₂,time = update(N,A₁,A₂,Ȧ₁,Ȧ₂,ω,η,Δx,Δt,time);
     end
@@ -228,13 +220,13 @@ function saving_function(N,t₀,t,A₁,A₂,Ȧ₁,Ȧ₂,ω,η,Δx,Δt)
     steps = round(t/Δt,digits = 0);
 
 
-    for i in 1:steps
+    for _ ∈ 1:steps
         time = round(time,digits = 1);
         if time % 1 == 0
             mod = sqrt.(A₁.^2 .+ A₂.^2);
             mod[mod .> 1] .= 1;
             save("plottting/"*lpad( string(trunc(Int,time-t₀)) ,3,"0")*".png", colorview(Gray,mod));
-            #save("gray.png",colorview(Gray,mod))
+
         end
     A₁,A₂,Ȧ₁,Ȧ₂,time = update(N, A₁, A₂, Ȧ₁, Ȧ₂, ω, η, Δx, Δt, time);
         
@@ -248,13 +240,12 @@ function saving(N,t₀,t,A₁,A₂,Ȧ₁,Ȧ₂,ω,η,Δx,Δt)
     steps = round(t/Δt,digits = 0);
 
 
-    for i in 1:steps
+    for _ ∈ 1:steps
         time = round(time,digits = 1);
         if time % 1 == 0
             mod = sqrt.(A₁.^2 .+ A₂.^2);
             mod[mod .> 1] .= 1;
             save("plottting/"*lpad( string(trunc(Int,time-t₀)) ,3,"0")*".png", colorview(Gray,mod));
-            #save("gray.png",colorview(Gray,mod))
         end
     A₁,A₂,Ȧ₁,Ȧ₂,time = update_2(N, A₁, A₂, Ȧ₁, Ȧ₂, ω, η, Δx, Δt, time);
         
@@ -268,13 +259,13 @@ function saving_roll(N,t₀,t,A₁,A₂,Ȧ₁,Ȧ₂,ω,η,Δx,Δt)
     steps = round(t/Δt,digits = 0);
 
 
-    for i in 1:steps
+    for _ ∈ 1:steps
         time = round(time,digits = 1);
         if time % 1 == 0
             mod = sqrt.(A₁.^2 .+ A₂.^2);
             mod[mod .> 1] .= 1;
             save("plottting/"*lpad( string(trunc(Int,time-t₀)) ,3,"0")*".png", colorview(Gray,mod));
-            #save("gray.png",colorview(Gray,mod))
+
         end
     A₁,A₂,Ȧ₁,Ȧ₂,time = update_3(N, A₁, A₂, Ȧ₁, Ȧ₂, ω, η, Δx, Δt, time);
         
@@ -288,13 +279,13 @@ function saving_circle(N,t₀,t,A₁,A₂,Ȧ₁,Ȧ₂,ω,η,Δx,Δt)
     steps = round(t/Δt,digits = 0);
 
 
-    for i in 1:steps
+    for _ ∈ 1:steps
         time = round(time,digits = 1);
         if time % 1 == 0
             mod = sqrt.(A₁.^2 .+ A₂.^2);
             mod[mod .> 1] .= 1;
             save("plottting/"*lpad( string(trunc(Int,time-t₀)) ,3,"0")*".png", colorview(Gray,mod));
-            #save("gray.png",colorview(Gray,mod))
+
         end
     A₁,A₂,Ȧ₁,Ȧ₂,time = update_4(N, A₁, A₂, Ȧ₁, Ȧ₂, ω, η, Δx, Δt, time);
         
@@ -302,41 +293,43 @@ function saving_circle(N,t₀,t,A₁,A₂,Ȧ₁,Ȧ₂,ω,η,Δx,Δt)
     return A₁,A₂
 end
 
-function saving_tensor(N,t₀,t,A₁,A₂,Ȧ₁,Ȧ₂,ω,η,Δx,Δt)
+function saving_laplace(N,t₀,t,A₁,A₂,Ȧ₁,Ȧ₂,ω,η,Δx,Δt)
     time = t₀;
     
     steps = round(t/Δt,digits = 0);
 
+    M₁ = Laplacian_circle(N,A₁,Δx)
+    M₂ = Laplacian_circle(N,A₂,Δx)
 
-    for i in 1:steps
+    for _ ∈ 1:steps
         time = round(time,digits = 1);
         if time % 1 == 0
             mod = sqrt.(A₁.^2 .+ A₂.^2);
             mod[mod .> 1] .= 1;
             save("plottting/"*lpad( string(trunc(Int,time-t₀)) ,3,"0")*".png", colorview(Gray,mod));
-            #save("gray.png",colorview(Gray,mod))
+
         end
-    A₁,A₂,Ȧ₁,Ȧ₂,time = update_5(N, A₁, A₂, Ȧ₁, Ȧ₂, ω, η, Δx, Δt, time);
+    A₁,A₂,Ȧ₁,Ȧ₂,M₁,M₂,time = update_7(N, A₁, A₂, Ȧ₁, Ȧ₂, M₁, M₂, ω, η, Δx, Δt, time);
         
     end
     return A₁,A₂
 end
 
-function saving_3D(N,t₀,t,A₁,A₂,Ȧ₁,Ȧ₂,ω,η,Δx,Δt)
+function saving_3D(N::Int16,t₀::Float16,t,A₁::AbstractArray{Float16},A₂::AbstractArray{Float16},Ȧ₁::AbstractArray{Float16},Ȧ₂::AbstractArray{Float16},ω::Float16,η::Float16,Δx::Float16,Δt::Float16)
     time = t₀;
     
     steps = round(t/Δt,digits = 0);
 
-
-    for i in 1:steps
+    P = zeros(N,N,N)
+    P = convert(Array{Float16},P)
+    for _ ∈ 1:steps
         time = round(time,digits = 1);
     #     # if time % 1 == 0
-    #     #     mod = sqrt.(A₁.^2 .+ A₂.^2);
-    #     #     mod[mod .> 1] .= 1;
-    #     #     #save("plottting_3D/"*lpad( string(trunc(Int,time-t₀)) ,3,"0")*".png", colorview(Gray,mod));
+
+    #     #     #save("plottting_3D/"*lpad( string(trunc(Int,time-t₀)) ,3,"0")*".png", colorview(Gray,sqrt.(A₁.^2 .+ A₂.^2)[sqrt.(A₁.^2 .+ A₂.^2) .> 1]));
     #     #     #save("gray.png",colorview(Gray,mod))
     #     # end
-        A₁,A₂,Ȧ₁,Ȧ₂,time = update_6(N, A₁, A₂, Ȧ₁, Ȧ₂, ω, η, Δx, Δt, time);
+        A₁,A₂,Ȧ₁,Ȧ₂,time = update_6(N, A₁, A₂, Ȧ₁, Ȧ₂, ω, η, Δx, Δt, time,P);
         
     end
     return A₁,A₂
